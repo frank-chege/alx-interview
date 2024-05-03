8b8f6ffd2cec2551df06f8936fbbd521a0bab48c
frankrazzih <francischege602@gmail.com>
2024-05-03 17:27:33 +0300

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3 
+def pascal_triangle(n):
+    #list of lists
+    triangle = [[1]]
+    counter = 0
+    #case where n <=0
+    if n <= 0:
+        return []
+    else:
+        #keep track of rows
+        for x in range(1, n):
+            list = [1] #initialise list with 1
+            try:
+                for index in range(1, x):
+                    #calculate the pascal value
+                    num = triangle[x-1][index-1] + triangle[x-1][index]
+                    list.append(num)
+            except:
+                break
+                
+            finally:
+                list.append(1)
+                triangle.append(list)#add list to the triangle
+                continue
+        return triangle
