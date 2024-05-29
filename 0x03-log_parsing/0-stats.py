ecbe3ef34a876c0f48c94b121e684f9c003be289
frankrazzih <francischege602@gmail.com>
2024-05-29 23:15:42 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,74 +1,40 @@
 #!/usr/bin/python3
-'''read from stdin and compute some metrics'''
+"""Performs log parsing from stdin"""
 
-import sys
-import ipaddress
-from dateutil.parser import parse
-import signal
 import re
+import sys
 
-#global dict containing the codes
-status_codes = {
-    'File size': 0,
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
-#compiled pattern
-pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*)\] "GET /projects/260 HTTP/1\.1" (\d{3,4}) (\d+)$'
-compiled_pat = re.compile(pattern)
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-def handler(signum, frame):
-    '''handles the keyboard interrupt signal'''
-    printer() #call printer function
 
-def printer():
-    '''print the dictionary'''
-    for key, value in status_codes.items():
-        if value == 0:
-            continue
-        else:
-            print(f'{key}: {value}')
+def print_stats(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
-def format_checker(line)->bool:
-    '''receives line read from stdin and check whether it matches the specified format'''
-    global status_codes
-    global compiled_pat
-    match = compiled_pat.match(line)
-    if match:
-        code = int(match.group(3))
-        size = int(match.group(4))
-        #update the status codes
-        status_codes['File size'] += size
-        status_codes[code] += 1
-        return True
-    else:
-        return False
 
-def main():
-    '''read from stdin'''
-    #setup the signal
-    signal.signal(signal.SIGINT, handler)
-    count = 0
+if __name__ == "__main__":
     try:
         for line in sys.stdin:
-            #check if the line format matches the specified one
-            if format_checker(line.strip()):
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                print_stats(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except Exception:
                 pass
-                count += 1
-                #check if count is
-            if count == 10:
-                printer() #call printing function
-                count = 0
-    finally:
-        #print any remaining lines
-        if count < 10:
-            printer()
-
-if __name__ == '__main__':
-    main()
+        print_stats(statusC_counter, file_size)
+    except KeyboardInterrupt:
+        print_stats(statusC_counter, file_size)
+        raise
