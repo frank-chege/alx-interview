bdfe029640f0b2e5ad915cc7db3fc3033f38a36f
frankrazzih <francischege602@gamil.com>
2024-06-05 01:29:00 +0300

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,37 +2,16 @@
 '''checks if a dataset is valid utf-8 encoded'''
 from typing import List
 
-#convert the data to bytes
-#check against the utf-8 rules using bitwise operators
-#1 byte char - 1st bit is 0
-# negate the first 4 bits
-#if 1st bit is 1 , it's a 1 byte char
-#count the no. of bytes ie bits == 8
-#if > 8, not valid utf-8
-#2 byte char - 1st byte bits are 11.. 2nd byte begins with 10
-#if first 2 bits are 0 and , it's a 2 byte char
-#count the no of bit ie bits == 16
-#3 byte char - 1st byte bits are 111.. 2nd byte begins with 10.. 3rd byte 10..
-#if first 3 bits are 0's , it's a 3-byte char
-#total bit == 24
-#4 byte char - 1st byte bits are 1111.. 2nd byte begins with 10.. 3rd byte 10.. 4th byte 10..
-#if first 4 bits are 0's, it's a 4-byte char
-#check if total bits == 32
-
 def validUTF8(data: List[int])->bool:
     '''checks if a dataset is valid UTF-8'''
-    #convert list into bytes
-    try:
-        byte_list = data
-    except:
-        return False
-    mask1 = 1 << 7
-    mask2 = 1 << 6
-    mask3 = 1 << 5
-    mask4 = 1 << 4
+    mask1 = 1
+    mask2 = 1 << 1
+    mask3 = 1 << 2
+    mask4 = 1 << 3
     #get each byte
-    for idx in range(0, len(byte_list)):
-        byte = byte_list[idx]
+    idx = 0
+    while idx < len(data):
+        byte = data[idx]
         #get the value of the bits using a bit mask
         bit1 = byte & mask1
         bit2 = byte & mask2
@@ -40,29 +19,44 @@ def validUTF8(data: List[int])->bool:
         bit4 = byte & mask4
         #1-byte char
         if bit1 == 0:
-            return True
+            idx += 1
+            continue
+        if bit2 == 0:
+            return False
         #2-byte char
-        elif bit1 == 1 and bit2 == 1 and bit3 == 0:
+        elif bit3 == 0:
             #check the 2nd byte
-            byte = byte_list[idx+1]
+            try:
+                byte = data[idx+1]
+            except:
+                return False
             byte2_bit1 = byte & mask1
             byte2_bit2 = byte & mask2
             if byte2_bit1 == 1 and byte2_bit2 == 0:
-                return True
+                idx += 2
+                continue
             else:
                 return False
         #3-byte char
-        elif bit1 == 1 and bit2 == 1 and bit3 == 1 and bit4 == 0:
+        elif bit3 == 1 and bit4 == 0:
             #check 2nd byte
+            try:
+                byte = data[idx+1]
+            except:
+                return False
             byte2_bit1 = byte & mask1
             byte2_bit2 = byte & mask2
             if byte2_bit1 == 1 and byte2_bit2 == 0:
                 #check 3rd byte
-                byte = byte_list[idx+2]
+                try:
+                    byte = data[idx+2]
+                except:
+                    return False
                 byte3_bit1 = byte & mask1
                 byte3_bit2 = byte & mask2
                 if byte3_bit1 == 1 and byte3_bit2 == 0:
-                    return True
+                    idx += 2
+                    continue
                 else:
                     return False
             else:
@@ -70,20 +64,28 @@ def validUTF8(data: List[int])->bool:
         #4-byte char
         elif bit1 == 1 and bit2 == 1 and bit3 == 1 and bit4 == 0:
             #check 2nd byte
+            try:
+                byte = data[idx+1]
+            except:
+                return False
             byte2_bit1 = byte & mask1
             byte2_bit2 = byte & mask2
             if byte2_bit1 == 1 and byte2_bit2 == 0:
                 #check 3rd byte
-                byte = byte_list[idx+2]
+                try:
+                    byte = data[idx+2]
+                except:
+                    return False
                 byte3_bit1 = byte & mask1
                 byte3_bit2 = byte & mask2
                 if byte3_bit1 == 1 and byte3_bit2 == 0:
                     #check 4th byte
-                    byte = byte_list[idx+3]
+                    byte = data[idx+3]
                     byte4_bit1 = byte & mask1
                     byte4_bit2 = byte & mask2
                     if byte4_bit1 == 1 and byte4_bit2 == 0:
-                        return True
+                        idx += 3
+                        continue
                     else:
                         return False
                 else:
