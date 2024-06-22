34ba1c7da17f56b8f68bd57406d667465ff8d6ec
frankrazzih <francischege602@gmail.com>
2024-06-22 22:05:44 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def makeChange(coins: list[int], total: int)->int:
+    '''get the minimum no. of coins required to achieve the total'''
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    sum = 0
+    count = 0
+    length = len(coins)
+    for idx in range(0, length):
+        while sum <= total:
+            count += 1
+            sum += coins[idx]
+            if sum == total:
+                return count
+        #remove last count and sum
+        count -= 1
+        sum -= coins[idx]
+    return -1
+        
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+get the minimum no. of coins required to achieve a certain total
