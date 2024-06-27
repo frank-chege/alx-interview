a4e31c14e47f5c35a4253687b081a8050020f82a
frankrazzih <francischege602@gmail.com>
2024-06-27 12:10:05 +0300

    calculate perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,120 +6,32 @@ def island_perimeter(grid):
     length = len(grid)
     width = 0
     height = 0
-    first = False
+    next = 0
+    prev = 0
     for row in range(0, length):
         for col in range(0, len(grid[row])):
-            if grid[row][col] == 1 and first is False:
-                #check for the first instance of 1
-                width += 1
-                height += 1
-                first = True
-            #check if next col is 1
-            if first:
-                count = 1
+            if grid[row][col] == 1:
+                #check if next and prev col == 1
                 try:
-                    next_w = grid[row][col+1]
-                    if next_w == 1:
-                        #this row has the width
-                        while True:
-                            try:
-                                #get width
-                                next = grid[row][col+count]
-                                if next == 1:
-                                    width += 1
-                                else:
-                                    #get height
-                                    count = 1
-                                    while True:
-                                        try:
-                                            next = grid[row+count][col]
-                                            if next == 1:
-                                                height += 1
-                                            else:
-                                                return (height*2) + (width*2)
-                                        except KeyError:
-                                            return (height*2) + (width*2)
-                                        count += 1
-                                    
-                            except KeyError:
-                                #get height
-                                count = 1
-                                while True:
-                                    try:
-                                        next = grid[row+count][col]
-                                        if next == 1:
-                                            height += 1
-                                        else:
-                                            return (height*2) + (width*2)
-                                    except KeyError:
-                                        return (height*2) + (width*2)
-                                    count += 1
-                            count += 1
-                    else:
-                        #get height
-                        while True:
-                            try:
-                                next = grid[row+count][col]
-                                if next == 1:
-                                    height += 1
-                                else:
-                                    #get width
-                                    row_count = count
-                                    count = 1
-                                    while True:
-                                        try:
-                                            next = grid[row+row_count][col+count]
-                                            if next == 1:
-                                                width += 1
-                                            else:
-                                                return (height*2) + (width*2)
-                                        except KeyError:
-                                            return (height*2) + (width*2)
-                                        count += 1
-                                    
-                            except KeyError:
-                                count = 1
-                                while True:
-                                    try:
-                                        next = grid[row][col+count]
-                                        if next == 1:
-                                            width += 1
-                                        else:
-                                            return (height*2) + (width*2)
-                                    except KeyError:
-                                        return (height*2) + (width*2)
-                                    count += 1
-                            count += 1
-                except KeyError:
-                    #this row has the height
-                    while True:
-                        try:
-                            next = grid[row+count][col]
-                            if next == 1:
-                                height += 1
+                    prev = grid[row][col-1]
+                except IndexError:
+                    pass
+                try:
+                    next = grid[row][col+1]
+                except IndexError:
+                    pass
+                if prev == 1 or next == 1:
+                    width += 1
+                #check for height
+                try:
+                    top = grid[row-1][col]
+                except IndexError:
+                    pass
+                try:
+                    bott = grid[row+1][col]
+                except IndexError:
+                    pass
+                if top == 1 or bott == 1:
+                    height += 1
+    return (height*2) + (width*2)
 
-                            else:
-                                count = 1
-                                while True:
-                                    try:
-                                        next = grid[row+count][col-count]
-                                        if next == 1:
-                                            width += 1
-                                        else:
-                                            return (height*2) + (width*2)
-                                    except KeyError:
-                                        return (height*2) + (width*2)
-                                    count += 1
-                        except KeyError:
-                            count = 1
-                            while True:
-                                try:
-                                    next = grid[row+count][col-count]
-                                    if next == 1:
-                                        width += 1
-                                    else:
-                                        return (height*2) + (width*2)
-                                except KeyError:
-                                    return (height*2) + (width*2)
-                                count += 1
-                        count += 1
