6dd6d1cd0b279f3200ae1a6bae413835701db9a9
frankrazzih <francischege602@gmail.com>
2024-07-05 15:37:17 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+'''find the winner of a primegame'''
 
 def is_prime(number):
-    '''Check if a number is prime'''
+    '''check if a no. is prime'''
     if number <= 1:
         return False
     if number <= 3:
@@ -14,52 +15,44 @@ def is_prime(number):
             return False
         i += 6
     return True
+    
 
-def generate_prime_list(n):
-    '''Generate a list of prime numbers up to n'''
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
-
-def play_game(n, primes):
-    '''Play one round of the game'''
-    set_numbers = set(range(1, n + 1))
-    turn = 0  # Maria starts (0: Maria, 1: Ben)
-
-    while True:
-        possible_moves = [p for p in primes if p in set_numbers]
-        if not possible_moves:
-            return turn  # Return the current turn as the loser
-
-        prime = possible_moves[0]
-        multiples = {prime * k for k in range(1, (n // prime) + 1)}
-        set_numbers -= multiples
-        turn = 1 - turn  # Switch turns
 
 def isWinner(x, nums):
-    '''Determine the winner after x rounds'''
+    '''primegame winner'''
+    prime = []
+    maria = True
     maria_score = 0
     ben_score = 0
-    max_n = max(nums)
-    primes = generate_prime_list(max_n)
-
-    for n in nums:
-        loser = play_game(n, primes)
-        if loser == 1:  # Ben loses, so Maria wins
-            maria_score += 1
-        else:  # Maria loses, so Ben wins
-            ben_score += 1
+    ben = False
+    for round in range(x):
+        for number in nums:
+            #create a list of int in range of the current integer
+            lis = [num for num in range(1, number+1)]
+            prime_list = []
+            for pr in lis:
+                if is_prime(pr):
+                    prime_list.append(pr)
+            for num in prime_list:
+                #remove the prime no.
+                lis.remove(num)
+                #remove its multiples
+                for n in lis:
+                    if n % num == 0:
+                        lis.remove(n)
+                if maria:
+                    maria_score += 1
+                    maria = False
+                else:
+                    ben_score += 1
+                    maria = True
 
     if maria_score > ben_score:
-        return 'Maria'
+        winner = 'Maria'
     elif ben_score > maria_score:
-        return 'Ben'
+        winner = 'Ben'
     else:
-        return None
+        winner = None
+    return winner
 
-# Example usage
-x = 3
-nums = [4, 5, 1]
-print(isWinner(x, nums))  # Output should be 'Ben'
+    
